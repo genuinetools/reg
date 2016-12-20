5b848b5b4fb439bf12552daadf6cc96e8eedd3bc
Jess Frazelle <acidburn@google.com>
2016-12-19 18:04:40 -0800

    server things
    Signed-off-by: Jess Frazelle <acidburn@google.com>

---
+++ server/server.go
@@ -0,0 +1,286 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"html/template"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
+
+	"github.com/Sirupsen/logrus"
+	"github.com/docker/docker/cliconfig"
+	"github.com/docker/engine-api/types"
+	"github.com/jessfraz/reg/registry"
+	"github.com/urfave/cli"
+)
+
+const (
+	// VERSION is the binary version.
+	VERSION = "v0.1.0"
+
+	dockerConfigPath = ".docker/config.json"
+)
+
+var (
+	updating = false
+)
+
+// preload initializes any global options and configuration
+// before the main or sub commands are run.
+func preload(c *cli.Context) (err error) {
+	if c.GlobalBool("debug") {
+		logrus.SetLevel(logrus.DebugLevel)
+	}
+
+	return nil
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "reg-server"
+	app.Version = VERSION
+	app.Author = "@jessfraz"
+	app.Email = "no-reply@butts.com"
+	app.Usage = "Docker registry v2 static UI server."
+	app.Before = preload
+	app.Flags = []cli.Flag{
+		cli.BoolFlag{
+			Name:  "debug, d",
+			Usage: "run in debug mode",
+		},
+		cli.StringFlag{
+			Name:  "username, u",
+			Usage: "username for the registry",
+		},
+		cli.StringFlag{
+			Name:  "password, p",
+			Usage: "password for the registry",
+		},
+		cli.StringFlag{
+			Name:  "registry, r",
+			Usage: "URL to the provate registry (ex. r.j3ss.co)",
+		},
+		cli.StringFlag{
+			Name:  "port",
+			Value: "8080",
+			Usage: "port for server to run on",
+		},
+		cli.StringFlag{
+			Name:  "cert",
+			Usage: "path to ssl cert",
+		},
+		cli.StringFlag{
+			Name:  "key",
+			Usage: "path to ssl key",
+		},
+		cli.StringFlag{
+			Name:  "interval",
+			Value: "5m",
+			Usage: "interval to generate new index.html's at",
+		},
+	}
+	app.Action = func(c *cli.Context) error {
+		auth, err := getAuthConfig(c)
+		if err != nil {
+			return err
+		}
+
+		// create the registry client
+		r, err := registry.New(auth, c.GlobalBool("debug"))
+		if err != nil {
+			return err
+		}
+
+		// get the path to the static directory
+		wd, err := os.Getwd()
+		if err != nil {
+			return err
+		}
+		staticDir := filepath.Join(wd, "static")
+
+		// create the initial index
+		if err := createStaticIndex(r, staticDir); err != nil {
+			return err
+		}
+
+		// parse the duration
+		dur, err := time.ParseDuration(c.String("interval"))
+		if err != nil {
+			logrus.Fatalf("parsing %s as duration failed: %v", c.String("interval"), err)
+		}
+		ticker := time.NewTicker(dur)
+
+		go func() {
+			// create more indexes every X minutes based off interval
+			for range ticker.C {
+				if !updating {
+					if err := createStaticIndex(r, staticDir); err != nil {
+						logrus.Warnf("creating static index failed: %v", err)
+					}
+				}
+			}
+		}()
+
+		// create mux server
+		mux := http.NewServeMux()
+
+		// static files handler
+		staticHandler := http.FileServer(http.Dir(staticDir))
+		mux.Handle("/", staticHandler)
+		// TODO: add handler for individual repos
+
+		// set up the server
+		port := c.String("port")
+		server := &http.Server{
+			Addr:    ":" + port,
+			Handler: mux,
+		}
+		logrus.Infof("Starting server on port %q", port)
+		if c.String("cert") != "" && c.String("key") != "" {
+			logrus.Fatal(server.ListenAndServeTLS(c.String("cert"), c.String("key")))
+		} else {
+			logrus.Fatal(server.ListenAndServe())
+		}
+
+		return nil
+	}
+
+	app.Run(os.Args)
+}
+
+func getAuthConfig(c *cli.Context) (types.AuthConfig, error) {
+	if c.GlobalString("username") != "" && c.GlobalString("password") != "" && c.GlobalString("registry") != "" {
+		return types.AuthConfig{
+			Username:      c.GlobalString("username"),
+			Password:      c.GlobalString("password"),
+			ServerAddress: c.GlobalString("registry"),
+		}, nil
+	}
+
+	dcfg, err := cliconfig.Load(cliconfig.ConfigDir())
+	if err != nil {
+		return types.AuthConfig{}, fmt.Errorf("Loading config file failed: %v", err)
+	}
+
+	// return error early if there are no auths saved
+	if !dcfg.ContainsAuth() {
+		if c.GlobalString("registry") != "" {
+			return types.AuthConfig{
+				ServerAddress: c.GlobalString("registry"),
+			}, nil
+		}
+		return types.AuthConfig{}, fmt.Errorf("No auth was present in %s, please pass a registry, username, and password", cliconfig.ConfigDir())
+	}
+
+	// if they passed a specific registry, return those creds _if_ they exist
+	if c.GlobalString("registry") != "" {
+		if creds, ok := dcfg.AuthConfigs[c.GlobalString("registry")]; ok {
+			return creds, nil
+		}
+		return types.AuthConfig{}, fmt.Errorf("No authentication credentials exist for %s", c.GlobalString("registry"))
+	}
+
+	// set the auth config as the registryURL, username and Password
+	for _, creds := range dcfg.AuthConfigs {
+		return creds, nil
+	}
+
+	return types.AuthConfig{}, fmt.Errorf("Could not find any authentication credentials")
+}
+
+func getRepoAndRef(c *cli.Context) (repo, ref string, err error) {
+	if len(c.Args()) < 1 {
+		return "", "", errors.New("pass the name of the repository")
+	}
+
+	arg := c.Args()[0]
+	parts := []string{}
+	if strings.Contains(arg, "@") {
+		parts = strings.Split(c.Args()[0], "@")
+	} else if strings.Contains(arg, ":") {
+		parts = strings.Split(c.Args()[0], ":")
+	} else {
+		parts = []string{arg}
+	}
+
+	repo = parts[0]
+	ref = "latest"
+	if len(parts) > 1 {
+		ref = parts[1]
+	}
+
+	return
+}
+
+type data struct {
+	RegistryURL string
+	LastUpdated string
+	Repos       []repository
+}
+
+type repository struct {
+	Name        string
+	Tags        string
+	RegistryURL string
+	// TODO: add date last uploaded
+}
+
+func createStaticIndex(r *registry.Registry, staticDir string) error {
+	updating = true
+	logrus.Info("fetching catalog")
+	repoList, err := r.Catalog()
+	if err != nil {
+		return fmt.Errorf("getting catalog failed: %v", err)
+	}
+
+	logrus.Info("fetching tags")
+	var repos []repository
+	for _, repo := range repoList {
+		tags, err := r.Tags(repo)
+		if err != nil {
+			return fmt.Errorf("getting tags for %s failed: %v", repo, err)
+		}
+		repos = append(repos, repository{
+			Name:        repo,
+			Tags:        strings.Join(tags, "<br/>"),
+			RegistryURL: r.URL,
+		})
+	}
+
+	// create temporoary file to save template to
+	logrus.Info("creating temporary file for template")
+	f, err := ioutil.TempFile("", "reg-server")
+	if err != nil {
+		return fmt.Errorf("creating temp file failed: %v", err)
+	}
+	defer f.Close()
+	defer os.Remove(f.Name())
+
+	// parse & execute the template
+	logrus.Info("parsing and executing the template")
+	templateDir := filepath.Join(staticDir, "../templates")
+	lp := filepath.Join(templateDir, "layout.html")
+
+	d := data{
+		RegistryURL: r.URL,
+		Repos:       repos,
+		LastUpdated: time.Now().String(),
+	}
+	tmpl := template.Must(template.New("").ParseFiles(lp))
+	if err := tmpl.ExecuteTemplate(f, "layout", d); err != nil {
+		return fmt.Errorf("execute template failed: %v", err)
+	}
+	f.Close()
+
+	logrus.Info("renaming the temporary file to index.html")
+	index := filepath.Join(staticDir, "index.html")
+	if err := os.Rename(f.Name(), index); err != nil {
+		return fmt.Errorf("renaming result from %s to %s failed: %v", f.Name(), index, err)
+	}
+	updating = false
+	return nil
+}
---
+++ server/static/js/scripts.js
@@ -0,0 +1,91 @@
+// pretty date function
+function prettyDate(time){
+	var date = new Date((time || "").replace(/-/g,"/").replace(/[TZ]/g," ")),
+		diff = (((new Date()).getTime() - date.getTime()) / 1000),
+		day_diff = Math.floor(diff / 86400);
+
+	if (isNaN(day_diff) || day_diff < 0)
+		return;
+
+	return day_diff == 0 && (
+			diff < 60 && "just now" ||
+			diff < 120 && "1 minute ago" ||
+			diff < 3600 && Math.floor( diff / 60 ) + " minutes ago" ||
+			diff < 7200 && "1 hour ago" ||
+			diff < 86400 && Math.floor( diff / 3600 ) + " hours ago") ||
+		day_diff == 1 && "Yesterday" ||
+		day_diff < 7 && day_diff + " days ago" ||
+		day_diff < 31 && Math.ceil( day_diff / 7 ) + " weeks ago" ||
+		day_diff > 31 && Math.round(day_diff / 31) + " months ago";
+}
+
+// search function
+function search(search_val){
+	var suche = search_val.toLowerCase();
+	var table = document.getElementById("directory");
+	var cellNr = 1;
+	var ele;
+	for (var r = 1; r < table.rows.length; r++){
+		ele = table.rows[r].cells[cellNr].innerHTML.replace(/<[^>]+>/g,"");
+		if (ele.toLowerCase().indexOf(suche)>=0 ) {
+			table.rows[r].style.display = '';
+		} else {
+			table.rows[r].style.display = 'none';
+		}
+	}
+}
+
+
+var el = document.querySelectorAll('tr:nth-child(2)')[0].querySelectorAll('td:nth-child(2)')[0];
+if (el.textContent == 'Parent Directory'){
+	var parent_row = document.querySelectorAll('tr:nth-child(2)')[0];
+	if (parent_row.classList){
+		parent_row.classList.add('parent');
+	} else {
+		parent_row.className += ' ' + 'parent';
+	}
+}
+
+// var rows = document.querySelectorAll('tr:not(.parent)');
+// Array.prototype.forEach.call(rows, function(item, index){
+// 	if (index !== 0) {
+// 		var date_holder = item.querySelectorAll('td:nth-child(3)')[0];
+// 		var date = date_holder.textContent;
+// 		date = prettyDate(date);
+// 		date_holder.innerHTML = date;
+// 	}
+// });
+
+var cells = document.querySelectorAll('td a');
+Array.prototype.forEach.call(cells, function(item, index){
+	var link = item.getAttribute('href');
+	link = link.replace('.html', '');
+	item.setAttribute('href', link);
+});
+
+var our_table = document.querySelectorAll('table')[0];
+our_table.setAttribute('id', 'directory');
+
+// search script
+var search_input = document.querySelectorAll('input[name="filter"]')[0];
+var clear_button = document.querySelectorAll('a.clear')[0];
+
+if (search_input.value !== ''){
+	search(search_input.value);
+}
+
+search_input.addEventListener('keyup', function(e){
+	e.preventDefault();
+	search(search_input.value);
+});
+
+search_input.addEventListener('keypress', function(e){
+	if ( e.which == 13 ) {
+		e.preventDefault();
+	}
+});
+
+clear_button.addEventListener('click', function(e){
+	search_input.value = '';
+	search('');
+});
