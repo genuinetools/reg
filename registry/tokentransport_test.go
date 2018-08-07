aa0d66cdc3785e3edd72373e77f9b5155f44bcdc
Jamie <jamie.roberts@accesscfa.com>
2018-08-07 08:37:58 -0400

    support 'access_token' for compatibility with OAuth 2.0  https://docs.docker.com/registry/spec/auth/token/#requesting-a-token

--- registry/tokentransport_test.go
+++ registry/tokentransport_test.go
@@ -3,6 +3,7 @@ package registry
 import (
 	"net/http"
 	"net/http/httptest"
+	"strings"
 	"testing"
 
 	"github.com/docker/docker/api/types"
@@ -34,5 +35,59 @@ func TestErrBasicAuth(t *testing.T) {
 	}
 	if token != "" {
 		t.Fatalf("expected empty token, got %v", err)
+	}
+}
+
+var authURI string
+
+func oauthFlow(w http.ResponseWriter, r *http.Request) {
+	if strings.HasPrefix(r.URL.Path, "/oauth2/accesstoken") {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"access_token":"abcdef1234"}`))
+		return
+	}
+	if strings.HasPrefix(r.URL.Path, "/oauth2/token") {
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(`{"token":"abcdef1234"}`))
+		return
+	}
+	auth := r.Header.Get("authorization")
+	if !strings.HasPrefix(auth, "Bearer") {
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		if authURI != "" {
+			w.Header().Set("www-authenticate", `Bearer realm="`+authURI+`/oauth2/token",service="my.endpoint.here"`)
+		}
+		w.WriteHeader(http.StatusUnauthorized)
+		w.Write([]byte(`{"errors":[{"code":"UNAUTHORIZED","message":"authentication required","detail":null}]}`))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+}
+
+func TestBothTokenAndAccessTokenWork(t *testing.T) {
+	ts := httptest.NewServer(http.HandlerFunc(oauthFlow))
+	defer ts.Close()
+
+	for _, which := range []string{"token", "accesstoken"} {
+		authURI = ts.URL + "/oauth2/" + which + "?service=my.endpoint.here"
+		authConfig := types.AuthConfig{
+			Username:      "abc",
+			Password:      "123",
+			ServerAddress: ts.URL,
+		}
+		authConfig.Email = "me@email.com"
+		r, err := New(authConfig, Opt{Insecure: true, Debug: true})
+		if err != nil {
+			t.Fatalf("expected no error creating client, got %v", err)
+		}
+		token, err := r.Token(ts.URL)
+		if err != nil {
+			t.Fatalf("err getting token from url: %v err: %v", ts.URL, err)
+		}
+		if token == "" {
+			t.Fatalf("error got empty token")
+		}
 	}
 }
