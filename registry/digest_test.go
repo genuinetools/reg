2bd71359e708deb5e9e1c4e5d882dde99ce23b85
Jess Frazelle <acidburn@jessfraz.com>
2019-05-25 18:01:15 -0400

    fix tests
    Signed-off-by: Jess Frazelle <acidburn@jessfraz.com>

--- registry/digest_test.go
+++ registry/digest_test.go
@@ -36,12 +36,12 @@ func TestDigestFromGCR(t *testing.T) {
 		t.Fatalf("Could not get auth config: %s", err)
 	}
 
-	r, err := New(ctx, auth, Opt{})
+	r, err := New(ctx, auth, Opt{SkipPing: true})
 	if err != nil {
 		t.Fatalf("Could not create registry instance: %s", err)
 	}
 
-	d, err := r.Digest(ctx, Image{Domain: "gcr.io", Path: "google_containers/hyperkube", Tag: "v1.9.9"})
+	d, err := r.Digest(ctx, Image{Domain: "gcr.io", Path: "google-containers/hyperkube", Tag: "v1.9.9"})
 	if err != nil {
 		t.Fatalf("Could not get digest: %s", err)
 	}
